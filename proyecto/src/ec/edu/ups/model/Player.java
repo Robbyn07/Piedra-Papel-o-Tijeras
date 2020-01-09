9c2919fc55f3669430c0e051dabee333516af84b
plojam <plojam@4d5d271d-5cd3-404e-bd3e-baeb75aab758>
2020-01-09 22:23:24 +0000

    Commit: Merge con trunk para mantener actualizado el proyecto de ramaPablo

---
+++ proyecto/src/ec/edu/ups/model/Player.java
@@ -0,0 +1,21 @@
+package ec.edu.ups.model;
+
+public class Player {
+	
+	private String name;
+	
+
+	public Player(String name) {
+		super();
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
