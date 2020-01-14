0db93254fb4937b161bd27a039ef221571fce6e6
plojam <plojam@4d5d271d-5cd3-404e-bd3e-baeb75aab758>
2020-01-14 00:37:28 +0000

    Commit: COntrol de bugs en las clases Sprite y SpriteSheet

--- Ppt/src/ec/edu/ups/view/graphics/Sprite.java
+++ Ppt/src/ec/edu/ups/view/graphics/Sprite.java
@@ -1,5 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package ec.edu.ups.view.graphics;
+
+import java.awt.image.BufferedImage;
 
 public class Sprite {
 
+	private final BufferedImage image;
+
+    private final int width;
+    private final int height;
+
+    public Sprite(final BufferedImage image) {
+        this.image = image;
+        this.width = image.getWidth();
+        this.height = image.getHeight();
+
+    }
+
+    public BufferedImage getImage() {
+        return image;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+	
 }
