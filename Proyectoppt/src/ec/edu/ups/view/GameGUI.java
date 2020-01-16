f9152d711b9e56f1c7a1ac1fae37934ac0221eba
Rakrad07 <Rakrad07@4d5d271d-5cd3-404e-bd3e-baeb75aab758>
2020-01-16 21:46:49 +0000

    Commit: Creacion de la clase Drawn e implementacion de metodos en GameGUI.

--- Proyectoppt/src/ec/edu/ups/view/GameGUI.java
+++ Proyectoppt/src/ec/edu/ups/view/GameGUI.java
@@ -4,13 +4,16 @@ import java.awt.Color;
 import java.awt.Graphics;
 
 import ec.edu.ups.main.Constants;
+import ec.edu.ups.model.Player;
+import ec.edu.ups.tools.Drawn;
 
 public class GameGUI {
 
 	private Graphics g;
+	private Player player;
 
 	public GameGUI() {
-		;
+
 	}
 
 	public void startPaint(Graphics g) {
@@ -20,11 +23,31 @@ public class GameGUI {
 
 	private void stadium() {
 
-		g.setColor(Color.WHITE);
-		g.fillRect(0, 0, Constants.WIDTH_WINDOW, Constants.HEIGHT_WINDOW);
+		// Base
+		Drawn.drawFilledRectangle(g, 0, 0, Constants.WIDTH_WINDOW, Constants.HEIGHT_WINDOW, Color.CYAN);
 
-		g.setColor(Color.CYAN);
-		g.fillOval((int) ((Constants.MID_WIDTH_WIN) - 64), (int) ((Constants.MID_HEIGHT_WIN) - 64), 128, 128);
+		// Figuras Complementarias
+		// Centro
+		Drawn.drawFilledCircle(g, Constants.MID_WIDTH_WIN - 8, Constants.MID_HEIGHT_WIN - 8, 16, 16, Color.BLACK);
+		Drawn.drawLine(g, Constants.MID_WIDTH_WIN, 0, Constants.MID_WIDTH_WIN, Constants.HEIGHT_WINDOW, Color.BLACK);
+		Drawn.drawCircumference(g, Constants.MID_WIDTH_WIN - 64, Constants.MID_HEIGHT_WIN - 64, 128, 128, Color.BLACK);
+		// Marcadores
+		Drawn.drawPerimeterRectangle(g, Constants.MID_WIDTH_WIN - 30, 0, 30, 30, Color.BLACK);
+		Drawn.drawPerimeterRectangle(g, Constants.MID_WIDTH_WIN, 0, 30, 30, Color.BLACK);
+
+		// Dibujar textos
+		// Marcadores
+		Drawn.drawString(g, "0", Constants.MID_WIDTH_WIN - 19, 19, Color.BLUE);
+		Drawn.drawString(g, "3", Constants.MID_WIDTH_WIN + 12, 19, Color.RED);
+		// Jugadores
+		Drawn.drawString(g, "Player 1", Constants.WIDTH_WINDOW - 665, Constants.HEIGHT_WINDOW - 50, Color.BLUE);
+		Drawn.drawString(g, "Player 2", Constants.WIDTH_WINDOW - 200, Constants.HEIGHT_WINDOW - 50, Color.RED);
+
+		/*
+		 * for (int i = 0; i < n; i++) { Drawn.drawElements(g,
+		 * player.getElements()[i].getImage(), player.getElements()[i].getPointX(),
+		 * player.getElements()[i].getPointY()); }
+		 */
 
 	}
 
