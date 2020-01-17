a06f780312380ce2327da1e6c64895d6a97d0def
czhizhpon <czhizhpon@4d5d271d-5cd3-404e-bd3e-baeb75aab758>
2020-01-17 01:29:15 +0000

    Commit: programacion de los metodos para pintar los sprites necesarios.

--- PptEduardo_v0.2/src/ec/edu/ups/controller/statemachine/game/GameRuleManager.java
+++ PptEduardo_v0.2/src/ec/edu/ups/controller/statemachine/game/GameRuleManager.java
@@ -2,13 +2,16 @@
 package ec.edu.ups.controller.statemachine.game;
 
 import java.awt.Graphics;
+import java.awt.image.BufferedImage;
 
 import ec.edu.ups.controller.RuleController;
 import ec.edu.ups.controller.statemachine.GameState;
 import ec.edu.ups.controller.statemachine.StateManager;
+import ec.edu.ups.main.Constants;
 import ec.edu.ups.model.Element;
 import ec.edu.ups.model.Player;
 import ec.edu.ups.view.GameGUI;
+import ec.edu.ups.view.graphics.SpriteSheet;
 
 public class GameRuleManager implements GameState {
 
@@ -25,22 +28,27 @@ public class GameRuleManager implements GameState {
 	private RuleController ruleController;
 	private GameGUI gameGui;
 
+	// Temporal
+	private SpriteSheet spriteSheetR;
+	private SpriteSheet spriteSheetP;
+	private SpriteSheet spriteSheetS;
+
 	public GameRuleManager() {
-		Element[] e1 = new Element[3];
-		Element[] e2 = new Element[3];
+		// Temporal
+		this.spriteSheetR = new SpriteSheet(Constants.ROCK_PATH, 64, false);
+		this.spriteSheetP = new SpriteSheet(Constants.PAPER_PATH, 64, false);
+		this.spriteSheetS = new SpriteSheet(Constants.SCISSORS_PATH, 64, false);
 
-		e1[0] = new Element(0, 0, null, false, 'r');
-		e1[1] = new Element(0, 0, null, false, 'p');
-		e1[2] = new Element(0, 0, null, false, 's');
+		// Fin Temporal
 
-		e2[0] = new Element(0, 0, null, false, 'r');
-		e2[1] = new Element(0, 0, null, false, 'p');
-		e2[2] = new Element(0, 0, null, false, 's');
+		player1 = new Player("Roby", getElementsPlayer1());
+		player2 = new Player("Edd", getElementsPlayer2());
 
-		player1 = new Player("Roby", e1);
-		player2 = new Player("Edd", e2);
 		ruleController = new RuleController(player1, player2);
-		gameGui = new GameGUI();
+		Player[] players = new Player[2];
+		players[0] = player1;
+		players[1] = player2;
+		gameGui = new GameGUI(players);
 		startState();
 	}
 
@@ -83,7 +91,61 @@ public class GameRuleManager implements GameState {
 
 	@Override
 	public void print(Graphics g) {
-		this.gameGui.startPaint(g);
+		this.gameGui.paint(g);
+	}
+
+	private Element[] getElementsPlayer1() {
+
+		Element[] elements = new Element[3];
+
+		int midMidW = Constants.MID_WIDTH_WIN / 2;
+		int midMidH = Constants.MID_HEIGHT_WIN;
+
+		int xR = midMidW;
+		int yR = midMidH - 128 - 64;
+
+		int xP = midMidW - 64 - 64;
+		int yP = midMidH - 64;
+
+		int xS = midMidW;
+		int yS = midMidH + 64;
+
+		BufferedImage imageR = this.spriteSheetR.getSprites(0).getImage();
+		BufferedImage imageP = this.spriteSheetP.getSprites(0).getImage();
+		BufferedImage imageS = this.spriteSheetS.getSprites(0).getImage();
+
+		elements[0] = new Element(xR, yR, imageR, false, 'R');
+		elements[1] = new Element(xP, yP, imageP, false, 'P');
+		elements[2] = new Element(xS, yS, imageS, false, 'S');
+		return elements;
+
+	}
+
+	private Element[] getElementsPlayer2() {
+
+		Element[] elements = new Element[3];
+
+		int midMidW = Constants.MID_WIDTH_WIN + (Constants.MID_WIDTH_WIN / 2) - 64;
+		int midMidH = Constants.MID_HEIGHT_WIN;
+
+		int xR = midMidW;
+		int yR = midMidH - 128 - 64;
+
+		int xP = midMidW + 64 + 64;
+		int yP = midMidH - 64;
+
+		int xS = midMidW;
+		int yS = midMidH + 64;
+
+		BufferedImage imageR = this.spriteSheetR.getSprites(0).getImage();
+		BufferedImage imageP = this.spriteSheetP.getSprites(0).getImage();
+		BufferedImage imageS = this.spriteSheetS.getSprites(0).getImage();
+
+		elements[0] = new Element(xR, yR, imageR, false, 'R');
+		elements[1] = new Element(xP, yP, imageP, false, 'P');
+		elements[2] = new Element(xS, yS, imageS, false, 'S');
+		return elements;
+
 	}
 
 }
--- PptEduardo_v0.2/src/ec/edu/ups/view/GameGUI.java
+++ PptEduardo_v0.2/src/ec/edu/ups/view/GameGUI.java
@@ -2,29 +2,67 @@ package ec.edu.ups.view;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.image.BufferedImage;
 
 import ec.edu.ups.main.Constants;
+import ec.edu.ups.model.Player;
+import ec.edu.ups.tools.Drawn;
 
 public class GameGUI {
 
 	private Graphics g;
+	private Player[] players;
 
-	public GameGUI() {
-		;
+	public GameGUI(Player[] players) {
+		this.players = players;
+
 	}
 
-	public void startPaint(Graphics g) {
-		this.g = g;
-		stadium();
-	}
+	public void paint(Graphics g) {
 
-	private void stadium() {
+		// Base
+		Drawn.drawFilledRectangle(g, 0, 0, Constants.WIDTH_WINDOW, Constants.HEIGHT_WINDOW, new Color(59, 6, 44));
 
-		g.setColor(Color.WHITE);
-		g.fillRect(0, 0, Constants.WIDTH_WINDOW, Constants.HEIGHT_WINDOW);
+		// Figuras Complementarias
+		// Centro
+		Drawn.drawFilledCircle(g, Constants.MID_WIDTH_WIN - 8, Constants.MID_HEIGHT_WIN - 8, 16, 16, Color.white);
+		Drawn.drawLine(g, Constants.MID_WIDTH_WIN, 0, Constants.MID_WIDTH_WIN, Constants.HEIGHT_WINDOW - 30,
+				Color.white);
+		Drawn.drawCircumference(g, Constants.MID_WIDTH_WIN - 64, Constants.MID_HEIGHT_WIN - 64, 128, 128, Color.white);
+		// Marcadores
 
-		g.setColor(Color.CYAN);
-		g.fillOval((int) ((Constants.MID_WIDTH_WIN) - 64), (int) ((Constants.MID_HEIGHT_WIN) - 64), 128, 128);
+		Drawn.drawFilledRectangle(g, Constants.MID_WIDTH_WIN - 30, 0, 60, 30, Color.white);
+		Drawn.drawFilledRectangle(g, Constants.MID_WIDTH_WIN - 30, Constants.HEIGHT_WINDOW - 30, 60, 30, Color.white);
+		Drawn.drawPerimeterRectangle(g, Constants.MID_WIDTH_WIN - 30, 0, 30, 30, Color.black);
+		Drawn.drawPerimeterRectangle(g, Constants.MID_WIDTH_WIN, 0, 30, 30, Color.black);
+
+		// Dibujar textos
+		// Marcadores
+		Drawn.drawString(g, "0", Constants.MID_WIDTH_WIN - 19, 19, Color.BLUE);
+		Drawn.drawString(g, "3", Constants.MID_WIDTH_WIN + 12, 19, Color.RED);
+		Drawn.drawString(g, "Ronda ", Constants.MID_WIDTH_WIN - 29, Constants.HEIGHT_WINDOW - 10, Color.black);
+		// Jugadores
+		Drawn.drawString(g, players[0].getName(), Constants.WIDTH_WINDOW - 665, Constants.HEIGHT_WINDOW - 50,
+				Color.BLUE);
+		Drawn.drawString(g, players[1].getName(), Constants.WIDTH_WINDOW - 200, Constants.HEIGHT_WINDOW - 50,
+				Color.RED);
+
+		int n = players.length;
+		int nE;
+		BufferedImage image;
+		int xE;
+		int yE;
+
+		for (int i = 0; i < n; i++) {
+			nE = players[i].getElements().length;
+			for (int j = 0; j < nE; j++) {
+				image = players[i].getElements()[j].getImage();
+				xE = players[i].getElements()[j].getPointX();
+				yE = players[i].getElements()[j].getPointY();
+				g.drawImage(image, xE, yE, null);
+			}
+
+		}
 
 	}
 
