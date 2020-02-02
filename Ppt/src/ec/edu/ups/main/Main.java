a0971e9c623ddb5f760aa0f7f9d5c41a3b11d725
bryansb <bryansb@4d5d271d-5cd3-404e-bd3e-baeb75aab758>
2020-02-02 22:58:57 +0000

    Commit: Depuracion de codigo.

--- Ppt/src/ec/edu/ups/main/Main.java
+++ Ppt/src/ec/edu/ups/main/Main.java
@@ -2,10 +2,12 @@ package ec.edu.ups.main;
 
 public class Main {
 
-	public static void main(String[] args) {
-		MainManager main = new MainManager("Paper, Rock and Scissors");
-		main.startGame();
-		main.mainLoop();
-	}
+    public static void main(String[] args) {
+	MainManager main = new MainManager("Paper, Rock and Scissors");
+	main.startGame();
+	main.mainLoop();
+    }
+
+    // Holi
 
 }
